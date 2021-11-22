307a6b8db24741d728207a6f20c0223d9c0e948d
rraj0811 <94861952+rraj0811@users.noreply.github.com>
2021-11-22 23:39:44 +0530

    Update index.js

--- URL-Shortner/index.js
+++ URL-Shortner/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-// import the model here
 const ShortURL = require('./models/url')
 
 app.set('view engine', 'ejs')
@@ -13,11 +12,11 @@ app.get('/', async (req, res) => {
 })
 
 app.post('/short', async (req, res) => {
-	// Grab the fullUrl parameter from the req.body
+	
 	const fullUrl = req.body.fullUrl
 	console.log('URL requested: ', fullUrl)
 
-	// insert and wait for the record to be inserted using the model
+	
 	const record = new ShortURL({
 		full: fullUrl
 	})
@@ -28,33 +27,33 @@ app.post('/short', async (req, res) => {
 })
 
 app.get('/:shortid', async (req, res) => {
-	// grab the :shortid param
+	
 	const shortid = req.params.shortid
 
-	// perform the mongoose call to find the long URL
+	
 	const rec = await ShortURL.findOne({ short: shortid })
 
-	// if null, set status to 404 (res.sendStatus(404))
+	
 	if (!rec) return res.sendStatus(404)
 
-	// if not null, increment the click count in database
+	
 	rec.clicks++
 	await rec.save()
 
-	// redirect the user to original link
+	
 	res.redirect(rec.full)
 })
 
-// Setup your mongodb connection here
+
 mongoose.connect('mongodb://localhost/codedamn', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 })
 
 mongoose.connection.on('open', async () => {
-	// Wait for mongodb connection before server starts
+	
 
-	// Just 2 URLs for testing purpose
+	
 	await ShortURL.create({ full: 'http://google.com', short: '5xr' })
 	await ShortURL.create({ full: 'http://codedamn.com' })
 
